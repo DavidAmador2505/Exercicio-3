50e64b6f59effb235ea59cd78d4f73ea9bbce448
David Amador <davidamador2505@gmail.com>
2023-11-29 11:36:52 +0000

    Add project files.

---
+++ exercicio 3/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace exercicio_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(36, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Isto é um rótulo";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(154, 36);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(211, 23);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "Isto é uma caixa de texto";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(36, 98);
+            button1.Name = "button1";
+            button1.Size = new Size(152, 94);
+            button1.TabIndex = 2;
+            button1.Text = "apagar texto/repor cor";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(216, 98);
+            button2.Name = "button2";
+            button2.Size = new Size(149, 94);
+            button2.TabIndex = 3;
+            button2.Text = "alterar cor de fundo";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(36, 210);
+            button3.Name = "button3";
+            button3.Size = new Size(152, 88);
+            button3.TabIndex = 4;
+            button3.Text = "repor texto";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(216, 210);
+            button4.Name = "button4";
+            button4.Size = new Size(149, 88);
+            button4.TabIndex = 5;
+            button4.Text = "Alterar cor da letra";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(124, 304);
+            button5.Name = "button5";
+            button5.Size = new Size(150, 72);
+            button5.TabIndex = 6;
+            button5.Text = "Alterar cor do fundo text box";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(396, 410);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+    }
+}
---
+++ exercicio 3/Form1.cs
@@ -0,0 +1,50 @@
+using System.Data;
+
+namespace exercicio_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            BackColor = default(Color);
+            textBox1.BackColor = default(Color);
+            textBox1.ForeColor = default(Color);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.DarkRed;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Isto é uma caixa de texto";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = Color.Gray;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.Black;
+        }
+    }
+}
